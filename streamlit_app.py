6158e3e4d43fe87432e2cfb1dbc940b9e5b5a018
Chaithradc <123303536+Chaithradc@users.noreply.github.com>
2023-08-17 19:40:07 +0530

    Update streamlit_app.py
    edited

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-pip install snowflake.connector
+import snowflake.connector
 
